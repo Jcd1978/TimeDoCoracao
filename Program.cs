4903419902721d5ec85abccb49cddf29d9d5018d
Meu Nome <joelma.carmo.duarte@gmail.com>
2026-03-27 16:01:27 -0300

    Exercício TimeDoCoracao encerrado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.Write("   P A L M E I R A S   ");
+Console.ResetColor();
+Console.WriteLine();
+
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write(" Vai ganhar o Mundial! ");
+Console.ResetColor();
